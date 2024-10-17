14b263e2b1724715d7a07f80950d4506bbe8f3b9
Ian Jiang <ianjiang@10-17-19-49.dynapool.wireless.nyu.edu>
2024-10-17 15:24:29 -0400

    finish api.py and add requirements

--- api.py
+++ api.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 import mysql  # MySQL connector to interact with the database
 from mysql.connector import Error  # Error handling for MySQL operations
 from db import *
+from pydantic import BaseModel
+
+class newperson(BaseModel):
+    name: str
+    title: str
+    industry: str
+    linkedin: str
+
 app = FastAPI()
 
 @app.get("/")
@@ -43,6 +51,41 @@ def all_persons():
         
     return json
 
+@app.post("/person/")
+def create_person(name, title, industry, linkedin):
+    #create connection
+    connection = None
+    json = {}
+    try:
+        # Attempt to connect using environment variables for DB credentials
+        connection = mysql.connector.connect(
+            host='34.44.42.132',
+            user='hirehelper',
+            password='hirehelper123!',
+            database='hirehelper'
+        )
+        print("Successfully connected to the database")
+    except Error as e:
+        # Print detailed error information if connection fails
+        print(f"Error connecting to MySQL: {e}")
+    # use add_person method from db.py
+    add_person(connection,name,title,industry,linkedin)
+    
+    # select and return the entry just added
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(f"SELECT * FROM person WHERE name = '{name}' AND title = '{title}' AND industry = '{industry}' AND linkedin = '{linkedin}'")
+            connection.commit()
+            results = cursor.fetchall()
+            if not results:
+                print("No person found in the database.")
+            else:
+                for person in results:
+                    json[person[0]] = {person[1],person[2],person[3],person[4]}
+    except Error as e:
+        print(f"Error retrieving users: {e}")
+    return json
+
 @app.get("/person/{person_id}")
 def person(person_id):
     #create connection
@@ -78,8 +121,41 @@ def person(person_id):
         
     return json
 
-@app.post("person/")
-def create_person(name, title, industry, linkedin):
+
+@app.put("/person/{person_id}")
+def update_person(person_id: int, name: str, title: str, industry: str, linkedin: str):
+    #create connection
+    connection = None
+    try:
+        # Attempt to connect using environment variables for DB credentials
+        connection = mysql.connector.connect(
+            host='34.44.42.132',
+            user='hirehelper',
+            password='hirehelper123!',
+            database='hirehelper'
+        )
+        print("Successfully connected to the database")
+    except Error as e:
+        # Print detailed error information if connection fails
+        print(f"Error connecting to MySQL: {e}")
+        
+    # Update the entry
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(f"SELECT * FROM person WHERE person_id = {person_id}")
+            results = cursor.fetchall()
+            if not results:
+                print("No person found in the database.")
+            else:
+                cursor.execute(f"UPDATE person SET name = '{name}', title = '{title}', industry = '{industry}', linkedin = '{linkedin}' WHERE person_id = {person_id}")
+                connection.commit()
+                return {f"Updated person {person_id}" : True}
+    except Error as e:
+        print(f"Error retrieving users: {e}")
+    return {f"Updated person {person_id}" : False}
+
+@app.delete("/person/{person_id}")
+def delete_person(person_id):
     #create connection
     connection = None
     json = {}
@@ -95,51 +171,20 @@ def create_person(name, title, industry, linkedin):
     except Error as e:
         # Print detailed error information if connection fails
         print(f"Error connecting to MySQL: {e}")
-    # use add_person method from db.py
-    add_person(connection,name,title,industry,linkedin)
-    
-    # select and return the entry just added
+    # delete
     try:
         with connection.cursor() as cursor:
-            cursor.execute(f"SELECT * FROM person WHERE name = {name} AND title = {title} AND industry = {industry} AND linkedin = {linkedin}")
+            #check if person exists
+            cursor.execute(f"SELECT * FROM person WHERE person_id = {person_id}")
             results = cursor.fetchall()
             if not results:
-                print("No person found in the database.")
+                print("person not found")
             else:
-                for person in results:
-                    json[person[0]] = {person[1],person[2],person[3],person[4]}
+                #if it exists, delete it
+                cursor.execute(f"DELETE FROM person WHERE person_id = {person_id}")
+                connection.commit()
+                return {"ok": True}
     except Error as e:
         print(f"Error retrieving users: {e}")
-    return json
+    return {"ok" : False}
     
-# @app.put("person/{person_id}")
-# def update_person(person_id):
-#     #create connection
-#     connection = None
-#     json = {}
-#     try:
-#         # Attempt to connect using environment variables for DB credentials
-#         connection = mysql.connector.connect(
-#             host='34.44.42.132',
-#             user='hirehelper',
-#             password='hirehelper123!',
-#             database='hirehelper'
-#         )
-#         print("Successfully connected to the database")
-#     except Error as e:
-#         # Print detailed error information if connection fails
-#         print(f"Error connecting to MySQL: {e}")
-        
-#     # Update the entry just added
-#     try:
-#         with connection.cursor() as cursor:
-#             cursor.execute(f"UPDATE person SET name = {name}, title = {title}, industry = {industry}, linkedin = {linkedin} ")
-#             results = cursor.fetchall()
-#             if not results:
-#                 print("No person found in the database.")
-#             else:
-#                 for person in results:
-#                     json[person[0]] = {person[1],person[2],person[3],person[4]}
-#     except Error as e:
-#         print(f"Error retrieving users: {e}")
-#     return json
